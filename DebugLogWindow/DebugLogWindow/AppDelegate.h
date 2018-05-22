d5b525fe3cf4ebd63de65ec50272d3d427386961
luochaojing <luochaojing@macs-iMac-2.local>
2018-05-22 17:10:18 +0800

    项目创建

---
+++ DebugLogWindow/DebugLogWindow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DebugLogWindow
+//
+//  Created by luochaojing on 2018/5/22.
+//  Copyright © 2018年 luochaojing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DebugLogWindow/DebugLogWindow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DebugLogWindow
+//
+//  Created by luochaojing on 2018/5/22.
+//  Copyright © 2018年 luochaojing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
